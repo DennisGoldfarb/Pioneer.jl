150165a45fc6a095a0d5b172e56d5e98018b5c10
DennisGoldfarb <goldfarb.dennis@wustl.edu>
2025-03-08 13:33:28 -0600

    snoop file for precompilation

---
+++ src/build/snoop.jl
@@ -0,0 +1,15 @@
+using Pioneer
+
+GetBuildLibParams("/Users/dennisgoldfarb/Downloads/Pioneer Testing/libraries/", 
+                    "yeast_prosit",
+                    "/Users/dennisgoldfarb/Downloads/Pioneer Testing/FASTA/")
+                    #params_path = "/Users/dennisgoldfarb/Downloads/build_test.json")
+
+GetSearchParams("/Users/dennisgoldfarb/Downloads/Pioneer Testing/libraries/yeast_prosit.poin",
+                "/Users/dennisgoldfarb/Downloads/Pioneer Testing/MTAC/arrow_out/",
+                "/Users/dennisgoldfarb/Downloads/Pioneer Testing/prosit_yeast_results/")
+                #params_path = "/Users/dennisgoldfarb/Downloads/search_test.json")
+
+BuildSpecLib("build/buildspeclib_params.json")
+SearchDIA("build/searchdia_params.json")
+convertMzML("data/mzML/")
