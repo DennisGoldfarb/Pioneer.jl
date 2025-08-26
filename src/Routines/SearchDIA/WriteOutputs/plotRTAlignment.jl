449f309e23e13fedcebba2c7d3b83eddf5a6fe6f
Dennis Goldfarb <Goldfarb.dennis@gmail.com>
2025-08-25 19:39:49 -0500

    Plot original and aligned iRTs

--- src/Routines/SearchDIA/WriteOutputs/plotRTAlignment.jl
+++ src/Routines/SearchDIA/WriteOutputs/plotRTAlignment.jl
@@ -15,9 +15,9 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-function plotRTAlign(RT::Vector{T}, 
-                    iRT::Vector{T}, 
-                    rt_map::Any; 
+function plotRTAlign(RT::Vector{T},
+                    iRT::Vector{T},
+                    rt_map::Any;
                     out_fdir::String = "./",
                     out_fname::String = "rt_align_plot") where {T<:AbstractFloat}
     n = length(RT)
@@ -32,29 +32,41 @@ function plotRTAlign(RT::Vector{T},
         end
         plot_title *= out_fname[i]
     end
-    n = length(RT)
-    p = Plots.plot(RT, iRT, seriestype=:scatter,
+
+    # Original library iRTs
+    p_orig = Plots.plot(RT, iRT, seriestype = :scatter,
                         title = plot_title*"\n n = $n",
                         xlabel = "Retention Time RT (min)",
-                        ylabel ="Indexed Retention Time iRT (min)",
+                        ylabel = "Indexed Retention Time iRT (min)",
                         label = nothing,
-                        size = 100*[13.3, 7.5]
-            ,
-            fontsize = 24,
-            titlefontsize = 24,
-            legendfontsize = 24,
-            tickfontsize = 24,
-            guidefontsize = 24,
-            margin = 10Plots.mm,
-            alpha = 0.1,
-            dpi = 300)
-    #=
-    Plots.plot!(p, (LinRange(minimum(RT), maximum(RT), 100)), 
-            rt_map.(LinRange(minimum(RT), maximum(RT), 100)),
-            lw = 6.0,
-            label = "RT Spline")
-    =#
-    savefig(p, joinpath(out_fdir, out_fname)*".pdf")
+                        size = 100*[13.3, 7.5],
+                        fontsize = 24,
+                        titlefontsize = 24,
+                        legendfontsize = 24,
+                        tickfontsize = 24,
+                        guidefontsize = 24,
+                        margin = 10Plots.mm,
+                        alpha = 0.1,
+                        dpi = 300)
+    savefig(p_orig, joinpath(out_fdir, out_fname)*"_original.pdf")
 
+    # Run-specific predicted iRTs using the alignment model
+    aligned_iRT = rt_map.(RT)
+    p_aligned = Plots.plot(RT, aligned_iRT, seriestype = :scatter,
+                        title = plot_title*"\n n = $n",
+                        xlabel = "Retention Time RT (min)",
+                        ylabel = "Indexed Retention Time iRT (min)",
+                        label = nothing,
+                        size = 100*[13.3, 7.5],
+                        fontsize = 24,
+                        titlefontsize = 24,
+                        legendfontsize = 24,
+                        tickfontsize = 24,
+                        guidefontsize = 24,
+                        margin = 10Plots.mm,
+                        alpha = 0.1,
+                        dpi = 300)
+    savefig(p_aligned, joinpath(out_fdir, out_fname)*"_aligned.pdf")
 
+    return p_orig, p_aligned
 end
